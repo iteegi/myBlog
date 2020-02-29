f321f5f86236127eb9cb9e7136be8268d806c4af
J D <jd@gmail.com>
2020-02-29 19:08:04 +0300

    Post class added to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
-from django.contrib import admin
+"""Post model configuration file on the administration site."""
 
-# Register your models here.
+from django.contrib import admin
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Configured the display of the post model on the administration site."""
+
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish')
