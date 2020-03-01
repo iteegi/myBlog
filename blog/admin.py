49d938a69d68355390a5121ab59b790c8deda901
J D <jd@gmail.com>
2020-03-02 00:56:47 +0300

    Comment model is registered on the administration site

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,12 @@
 """Post model configuration file on the administration site."""
 
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    """Configured the display of the post model on the administration site."""
+    """Configured display of the post model on the administration site."""
 
     list_display = ('title', 'slug', 'author', 'publish', 'status')
     list_filter = ('status', 'created', 'publish', 'author')
@@ -15,3 +15,12 @@ class PostAdmin(admin.ModelAdmin):
     raw_id_fields = ('author',)
     date_hierarchy = 'publish'
     ordering = ('status', 'publish')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """Configured display of the comment model on the administration site."""
+
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
