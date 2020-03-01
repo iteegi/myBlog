61ddd3ba67c6331972c5a521a1021086192af587
J D <jd@gmail.com>
2020-03-01 15:33:24 +0300

    view for email form added

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from django.core.mail import send_mail
 
+from .forms import EmailPostForm
 from .models import Post
 
 
@@ -43,3 +45,27 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    """Process request for email form."""
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    if request.method == 'POST':  # The form has been submitted.
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+
+            subject = '{}({}) recommends you reading "{}"'.format(
+                cd['name'], cd['email'], post.title)
+
+            message = 'Read "{}" at {}\n\n{}\'s comments:{}'.format(
+                post.title, post_url, cd['name'], cd['comments'])
+
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            sent = True
+        else:
+            form = EmailPostForm()
+            return render(request, 'blog/post/share.html',
+                          {'post': post, 'form': form})
