b2a1431e80566c0234b4357fcb064763cfd53c0e
J D <jd@gmail.com>
2020-03-03 00:33:43 +0300

    markdawn filter added

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -3,6 +3,9 @@
 from django.db.models import Count
 from django import template
 from ..models import Post
+from django.utils.safestring import mark_safe
+
+import markdown
 
 register = template.Library()
 
@@ -25,3 +28,9 @@ def get_most_commented_posts(count=5):
     """Get the article with the most comments."""
     return Post.published.annotate(total_comments=Count('comments'))\
         .order_by('-total_comments')[:count]
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    """Populate article body with Markdown formatting."""
+    return mark_safe(markdown.markdown(text))
